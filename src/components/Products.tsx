3ce8e6134987a2fab43e693914191c48d10d0774
praesidi <edik.halilov1999@gmail.com>
2024-03-06 21:02:42 +0300

    remove duplicates inside Products component

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -2,14 +2,22 @@ import { Product } from '@/types';
 import ProductCard from './ProductCard';
 import NotFound from './NotFound';
 
+function removeDuplicates(array: Product[]) {
+	return array.filter(
+		(value, index, self) => index === self.findIndex((t) => t.id === value.id),
+	);
+}
+
 export default function Products({ data }: { data: Product[] | null }) {
+	const filteredData = data && removeDuplicates(data);
+
 	return (
 		<>
 			{data === null || data.length === 0 ? (
 				<NotFound />
 			) : (
 				<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4'>
-					{data?.map((item) => (
+					{filteredData?.map((item) => (
 						<ProductCard
 							key={item.id}
 							product={item.product}
