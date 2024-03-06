3065882f2ca0b493e955c50a716cbc35c0334e4e
praesidi <edik.halilov1999@gmail.com>
2024-03-06 20:53:05 +0300

    add hook to fetch list of brands

---
+++ src/hooks/useFetchBrands.ts
@@ -0,0 +1,49 @@
+import api from '@/lib/axiosInstance';
+import { useEffect, useState } from 'react';
+import { Brands } from '@/types';
+
+export default function useFetchBrands() {
+	const [data, setData] = useState<(string | null)[] | null>(null);
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const [error, setError] = useState<string | null>(null);
+	// const maxRetries = 3;
+	// const retryDelay = 1000;
+
+	useEffect(() => {
+		// let retries = 0;
+		setIsLoading(true);
+
+		async function fetchData() {
+			console.log('fetch brands');
+
+			try {
+				const response = await api.post('', {
+					'action': 'get_fields',
+					'params': {
+						'field': 'brand',
+					},
+				});
+				const data: Brands = await response.data.result;
+				const filtered = [...new Set(data)];
+				setData(filtered);
+			} catch (error) {
+				// if (retries < maxRetries) {
+				// 	retries++;
+				// 	setTimeout(fetchData, retryDelay);
+				// } else {
+				// 	setError(
+				// 		`Something went wrong while getting data about brands: ${error}`,
+				// 	);
+				// }
+
+				setError(
+					`Something went wrong while getting data about brands: ${error}`,
+				);
+			}
+			setIsLoading(false);
+		}
+		fetchData();
+	}, []);
+
+	return { data, isLoading, error };
+}
