ee4315c5ea5b8ada9d506ae652b77435a52edab4
praesidi <edik.halilov1999@gmail.com>
2024-03-07 20:13:04 +0300

    fix deployment error

--- src/variables.ts
+++ src/variables.ts
@@ -1,5 +1,5 @@
 const API_PASSWORD = 'Valantis';
-const API_URL = 'http://api.valantis.store:40000/';
+const API_URL = 'https://api.valantis.store:41000/';
 const PRODUCTS_PER_PAGE = 50;
 
 export { API_PASSWORD, API_URL, PRODUCTS_PER_PAGE };
